7ddb6ca3e70a85355dbea0a3b8a8660542e9aa27
kurlayusha <tbark.guard@gmail.com>
2023-04-03 13:58:02 +0300

    lights done

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -7,8 +7,40 @@ type contentProps = {
 }
 
 function Content({ data }: contentProps) {
+  function handleMouseMove(e: any) {
+    const { currentTarget: target } = e
+
+    const rect = target.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    target.style.setProperty('--mouse-x', `${x}px`)
+    target.style.setProperty('--mouse-y', `${y}px`)
+  }
+
+  // in this function, I want to get the id of the card that is being hovered
+  // and then apply the hover effects to the nearby cards
+  function handleMouseMoveTwo(e: any) {
+    // if there is no card being hovered, then return
+    if (e.target.id === '') {
+      return
+    } else {
+      for (const card of e.currentTarget.children) {
+        const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top
+
+        card.style.setProperty('--mouse-x', `${x}px`)
+        card.style.setProperty('--mouse-y', `${y}px`)
+      }
+    }
+  }
+
   return (
-    <div className="contents">
+    <div
+      className="contents"
+      onMouseMove={(e) => handleMouseMoveTwo(e)}
+    >
       {data.map((project: any) => (
         <div
           className="content--card"
@@ -17,6 +49,7 @@ function Content({ data }: contentProps) {
           style={{
             backgroundImage: `url(${project.image[0]})`,
           }}
+          onMouseMove={(e) => handleMouseMove(e)}
         >
           <div className="card--info">
             <div className="bar-left">
